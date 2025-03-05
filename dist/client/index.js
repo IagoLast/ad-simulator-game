ba949c223cf7dfd3fc69c958b8b6ab995489cef4
IagoLast <iago.lastra@gmail.com>
2025-03-05 14:02:14 +0100

    chore

--- dist/client/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Game_1 = require("./game/Game");
-window.addEventListener('DOMContentLoaded', () => {
-    const game = new Game_1.Game();
-});
-//# sourceMappingURL=index.js.map
