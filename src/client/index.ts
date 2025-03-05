a098a6e39ff262764dfe4676b8ad170747a1cf51
IagoLast <iago.lastra@gmail.com>
2025-03-05 20:48:47 +0100

    fix

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,5 +3,7 @@ import { Game } from './game/Game';
 // Initialize the game when the DOM is fully loaded
 window.addEventListener('DOMContentLoaded', () => {
   // Create the game (constructor starts animation)
-  const game = new Game();
+  if(window.location.pathname !== '/') {
+    const game = new Game();
+  }
 }); 
